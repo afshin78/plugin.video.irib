0e13db98f06749bf1e76cdfbecf26937e4982488
afshin78 <afshin@tarza.de>
2016-09-14 19:37:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,61 +2,61 @@
 # -*- coding: utf-8 -*-
 # Module: default
 # Author: Roman V. M.
-# Created on: 28.11.2014
+# Created on: 12.09.2016
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 
 from xbmcswift2 import Plugin
 
 IRIB_CHANNEL_STREAMS = (
-  {'name': 'Channel 1',
+  {'name': 'شبکه یک',
   'logo': 'nasim.png',
   'stream_url': 'rtmp://live.video.asandl.com/devices//tv1-1000k.stream',
   },
-  {'name': 'Channel 2',
+  {'name': 'شبکه دو',
   'logo': 'nasim.png',
   'stream_url': 'rtmp://live.video.asandl.com/devices//tv2-1000k.stream',
   },
-  {'name': 'Channel 3',
+  {'name': 'شبکه سه',
   'logo': 'nasim.png',
   'stream_url': 'rtmp://live.video.asandl.com/devices//tv3-1000k.stream',
   },
-  {'name': 'Channel 4',
+  {'name': 'شبکه چهار',
   'logo': 'nasim.png',
   'stream_url': 'rtmp://live.video.asandl.com/devices//tv4-1000k.stream',
   },
-  {'name': 'Tehran',
+  {'name': 'شبکه تهران',
   'logo': 'nasim.png',
   'stream_url': 'rtmp://live.video.asandl.com/devices//tehran-1000k.stream',
   },
-  {'name': 'IRINN',
+  {'name': 'شبکه خبر',
   'logo': 'nasim.png',
   'stream_url': 'rtmp://live.video.asandl.com/devices//irinn-1000k.stream',
   },
-  {'name': 'Pooya',
+  {'name': 'پویا',
   'logo': 'nasim.png',
   'stream_url': 'rtmp://live.video.asandl.com/devices//pooya-1000k.stream',
   },
-  {'name': 'Namayesh',
+  {'name': 'نمایش',
   'logo': 'nasim.png',
   'stream_url': 'rtmp://live.video.asandl.com/devices//namayesh-1000k.stream',
   },
-  {'name': 'Varzesh',
+  {'name': 'ورزش',
   'logo': 'nasim.png',
   'stream_url': 'rtmp://live.video.asandl.com/devices//varzesh-1000k.stream',
   },
-  {'name': 'Mostanad',
+  {'name': 'مستند',
   'logo': 'nasim.png',
   'stream_url': 'rtmp://live.video.asandl.com/devices//mostanad-500k.stream',
   },
-  {'name': 'Salamat',
+  {'name': 'سلامت',
   'logo': 'nasim.png',
   'stream_url': 'rtmp://live.video.asandl.com/devices//salamat-500k.stream',
   },
-  {'name': 'Nasim',
+  {'name': 'نسیم',
   'logo': 'nasim.png',
   'stream_url': 'rtmp://live.video.asandl.com/devices//nasim-1000k.stream',
   },
-  {'name': 'Kordestan',
+  {'name': 'کردستان',
   'logo': 'nasim.png',
   'stream_url': 'rtmp://live.video.asandl.com/devices//kordestan-300k.stream',
   },
@@ -66,14 +66,6 @@ plugin = Plugin()
 
 @plugin.route('/')
 def show_root_menu():
-  items = [
-    {'label': 'IRIB',
-    'path': plugin.url_for('show_channels')},
-  ]
-  return plugin.finish(items)
-
-@plugin.route('/IRIB/')
-def show_channels():
   items = [{
     'label': channel['name'],
     'thumbnail': get_logo(channel['logo']),
@@ -84,7 +76,7 @@ def show_channels():
 
 def get_logo(logo):
   addon_id=plugin._addon.getAddonInfo('id')
-  return 'special://home/addons/%s/resources/%s' % (addon_id, logo)
+  return 'special://home/addons/%s/resources/media/%s' % (addon_id, logo)
 
 def log(text):
   plugin.log.info(text)
